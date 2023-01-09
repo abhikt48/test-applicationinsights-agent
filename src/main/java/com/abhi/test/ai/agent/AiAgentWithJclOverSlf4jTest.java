267af5f09489c21e336a55399008457e78a2ec10
Abhishek Kumar <abhikt48@gmail.com>
2023-01-09 15:04:37 +0000

    Sample code to generate logs

---
+++ src/main/java/com/abhi/test/ai/agent/AiAgentWithJclOverSlf4jTest.java
@@ -0,0 +1,15 @@
+package com.abhi.test.ai.agent;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class AiAgentWithJclOverSlf4jTest{
+
+	
+	private static final Log logger = LogFactory.getLog(AiAgentWithJclOverSlf4jTest.class);
+	
+	public static void main(String[] args) {
+		logger.info("** Hello World By JCL Over SLF4j ");
+	}
+
+}
---
+++ src/main/java/com/abhi/test/ai/agent/AiAgentWithSlf4jTest.java
@@ -0,0 +1,14 @@
+package com.abhi.test.ai.agent;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AiAgentWithSlf4jTest{
+
+	private static final Logger logger = LoggerFactory.getLogger(AiAgentWithSlf4jTest.class);
+	
+	public static void main(String[] args) {
+		logger.info("** Hello World By Slf4j **");
+	}
+
+}
